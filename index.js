2c047d380b611d5471561b7ca9f39d494440e0a8
Davi <112717111+DaviJust@users.noreply.github.com>
2023-02-08 13:07:31 -0300

    Criação da array que servirá de pote
    Vira um pote, pois dentro terá vários atributos registrados e lidos pelo readline.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const readlineSync = require ('readline-sync'); // import da biblioteca readline sync 
+const readlineSync = require ('readline-sync'); 
+// import da biblioteca readline sync 
+const propri = [] 
+// Cria um array vazio para armazenar as propriedades, servira de pote para guarda os registros das propriedades que serão retornadas pelo readline
+
