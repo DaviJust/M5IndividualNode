0371b832a28bdd9832aec4003c7878c36049306e
Davi <112717111+DaviJust@users.noreply.github.com>
2023-02-08 13:10:17 -0300

    Criação da entrada
    Essa entrada dá a interação do usuário para as funções que usaram o readline

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const readlineSync = require ('readline-sync');
 // import da biblioteca readline sync 
 const propri = [] 
 // Cria um array vazio para armazenar as propriedades, servira de pote para guarda os registros das propriedades que serão retornadas pelo readline
-
+let input = "";
+ // O input é a entrada e servirá  como um comando para terminar os registros das propriedades, ou para ver as propriedades registradas, ou registrar as propriedades.
