66c509336db8193700a10c28670b252fb7e93962
Davi <112717111+DaviJust@users.noreply.github.com>
2023-02-10 12:45:11 -0300

    Pegar atributos
    Função que pega os atributos que são registrados pelo o usuário

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ const lista = [];
 let entrada = "";
 // É a entrada que servirá  como um comando para terminar os registros das propriedades, ou para ver as propriedades registradas.
 //OBS:Será a porta que o usuário entrará e sairá
+
+const adicionarNaLista = (prop) => {
+  lista.push(prop);
+  //Dentro da array serão adicionadas a leitura das propriedades do CSS, ou seja: A cada nova propriedade será computada um novo registro
+
+};
