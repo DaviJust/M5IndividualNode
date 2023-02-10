03564a1797f531e7063fb7d3fee8bf7eb2f152f6
Davi <112717111+DaviJust@users.noreply.github.com>
2023-02-10 12:44:32 -0300

    Refatoração de código
    Código foi refatorado para entrar nos padrões do trabalho

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const readlineSync = require ('readline-sync'); 
-// import da biblioteca readline sync 
-const propri = [] 
-// Cria um array vazio para armazenar as propriedades, servira de pote para guarda os registros das propriedades que serão retornadas pelo readline
-let input = "";
- // O input é a entrada e servirá  como um comando para terminar os registros das propriedades, ou para ver as propriedades registradas, ou registrar as propriedades.
+const redlineSync = require("readline-sync");
+// import da biblioteca readline sync
+const lista = [];
+// Cria um array vazio para armazenar as propriedades.
+// A lista Servira de pote para guarda os registros das propriedades que serão retornadas pelo readline
+let entrada = "";
+// É a entrada que servirá  como um comando para terminar os registros das propriedades, ou para ver as propriedades registradas.
+//OBS:Será a porta que o usuário entrará e sairá
